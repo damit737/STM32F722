a9af204cd16bb9645c250b95dbc6b5dce7fd7cf8
ShihWei <damit737@winstar.com.tw>
2023-08-02 13:18:50 +0800

    Change the GPIO function of SW8 from output to input

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -626,7 +626,7 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOD_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOC, SW8_Pin|ERD_Pin|HPMb_Pin, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(GPIOC, ERD_Pin|HPMb_Pin, GPIO_PIN_SET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(CTRL_Char_GPIO_Port, CTRL_Char_Pin, GPIO_PIN_RESET);
@@ -640,14 +640,16 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(DCX_GPIO_Port, DCX_Pin, GPIO_PIN_SET);
 
-  /*Configure GPIO pins : SW5_Pin SW6_Pin SW7_Pin SW3_Pin */
-  GPIO_InitStruct.Pin = SW5_Pin|SW6_Pin|SW7_Pin|SW3_Pin;
+  /*Configure GPIO pins : SW5_Pin SW6_Pin SW7_Pin SW8_Pin
+                           SW3_Pin */
+  GPIO_InitStruct.Pin = SW5_Pin|SW6_Pin|SW7_Pin|SW8_Pin
+                          |SW3_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : SW8_Pin ERD_Pin HPMb_Pin */
-  GPIO_InitStruct.Pin = SW8_Pin|ERD_Pin|HPMb_Pin;
+  /*Configure GPIO pins : ERD_Pin HPMb_Pin */
+  GPIO_InitStruct.Pin = ERD_Pin|HPMb_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
